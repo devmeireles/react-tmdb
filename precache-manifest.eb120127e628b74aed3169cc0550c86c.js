36c887b6a90b04a4d855ca8b1afa20e04dcd72da
Gabriel Meireles <dev.meireles@gmail.com>
2019-05-28 17:43:53 -0300

    Updates

---
+++ precache-manifest.eb120127e628b74aed3169cc0550c86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2323a4385b7afbfd0c3c0aa11e7293",
+    "url": "/react-tmdb/index.html"
+  },
+  {
+    "revision": "6f6e23027a0313b25ab1",
+    "url": "/react-tmdb/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6b1a0dfe3634d45f9bed",
+    "url": "/react-tmdb/static/css/main.7f8dc550.chunk.css"
+  },
+  {
+    "revision": "6f6e23027a0313b25ab1",
+    "url": "/react-tmdb/static/js/2.0beb6455.chunk.js"
+  },
+  {
+    "revision": "6b1a0dfe3634d45f9bed",
+    "url": "/react-tmdb/static/js/main.fe6bf540.chunk.js"
+  },
+  {
+    "revision": "677d9621d18973333028",
+    "url": "/react-tmdb/static/js/runtime~main.9614d2f4.js"
+  }
+]);
