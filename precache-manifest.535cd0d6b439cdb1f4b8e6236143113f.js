eabf60907903ee7d4f2d6c1695c00fc3317e7f6a
Gabriel Meireles <dev.meireles@gmail.com>
2019-06-03 16:39:18 -0300

    Updates

---
+++ precache-manifest.535cd0d6b439cdb1f4b8e6236143113f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299175b1a2dc7a28e9034830e0c37729",
+    "url": "/react-tmdb/index.html"
+  },
+  {
+    "revision": "9dc81a9fe66218c8c573",
+    "url": "/react-tmdb/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "625f335ebb3fc6b5eb32",
+    "url": "/react-tmdb/static/css/main.f8cd4703.chunk.css"
+  },
+  {
+    "revision": "9dc81a9fe66218c8c573",
+    "url": "/react-tmdb/static/js/2.ccbfd4f5.chunk.js"
+  },
+  {
+    "revision": "625f335ebb3fc6b5eb32",
+    "url": "/react-tmdb/static/js/main.4a3ffaeb.chunk.js"
+  },
+  {
+    "revision": "677d9621d18973333028",
+    "url": "/react-tmdb/static/js/runtime~main.9614d2f4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tmdb/precache-manifest.1b210a8eda45c592dfc372a23f2f1ef5.js"
+  "/react-tmdb/precache-manifest.535cd0d6b439cdb1f4b8e6236143113f.js"
 );
 
 self.addEventListener('message', (event) => {
